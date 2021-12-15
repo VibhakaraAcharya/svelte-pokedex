899d1ed26cf57da9a01122ca955fe19cc635170c
Vibhakara Acharya <vibhakara.acharya@nulurn.net>
2021-12-15 16:43:39 +0530

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>About Svelte Kit Pokedex</title>
+</svelte:head>
+
+
+<h1 class="text-4xl text-center  my-8 uppercase">About</h1>
+<p>This is cool svelte kit demo that uses pokemon api</p>
---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+    export async function load({page}) {
+        const id = page.params.id;
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const res = await fetch(url)
+        const pokeman = await res.json()
+        return {props: {pokeman}}
+    }
+</script>
+
+<script>
+    export let pokeman;
+    const type = pokeman.types[0].type.name
+</script>
+
+<div class="flex flex-col items-center">
+
+<h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+<p class="text-4xl">
+
+    Type: <strong>{type.toUpperCase()}</strong> | Height: <strong>{pokeman.height}</strong> | Weight: <strong>{pokeman.height}</strong>
+
+</p>
+<img class="card-image h-40 w-40" src={pokeman.sprites['front_default']} alt={pokeman.name}>
+</div>
